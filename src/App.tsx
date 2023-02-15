7ab0641cfc7e0796c85410e1d770072d19d67560
Oruj Garaev <oruc.qarayev90@gmail.com>
2023-02-15 23:40:47 +0400

    test commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
 import { FC } from "react";
 import { Provider } from "react-redux";
 import "./App.css";
 import Home from "./componetns/Home";
 import { store } from "./store";
-import { todosService } from "./store/servers/todosService";
 
 const App: FC = () => {
   return (
