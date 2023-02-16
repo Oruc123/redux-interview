8be80dbd99b30882b89f3826ed2ed06e6a9aa959
Oruj Garaev <oruc.qarayev90@gmail.com>
2023-02-16 07:46:40 +0400

    sd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Provider } from "react-redux";
 import "./App.css";
 import Home from "./componetns/Home";
 import { store } from "./store";
-
+const secret = "sdsa";
 const App: FC = () => {
+  useEffect(() => {
+    console.log("sda");
+  });
   return (
     <>
       <Provider store={store}>
